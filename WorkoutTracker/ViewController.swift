47bce26a8a98031635d5374632c9e2654ac073f3
Pavelweb22 <pavelweb2016@yandex.ru>
2022-10-04 20:44:09 +0300

    Initial Commit

---
+++ WorkoutTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkoutTracker
+//
+//  Created by Павел Яковенко on 04.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
